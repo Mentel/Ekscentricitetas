ebe39c832826644e61292769af56f3a7f076a6ed
Nojus Rimeisis <n.rimeisis@gmail.com>
2019-12-18 04:33:52 +0200

    RentV2

--- WindowsFormsApp1/Rent_addForm.Designer.cs
+++ WindowsFormsApp1/Rent_addForm.Designer.cs
@@ -28,37 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.RentClientComboBox = new System.Windows.Forms.ComboBox();
+            this.RentWorkerComboBox = new System.Windows.Forms.ComboBox();
+            this.NewRentPriceLine = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.RentAddConfirmButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.RentAddClientLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.RentAddWorkerLabel = new MaterialSkin.Controls.MaterialLabel();
             this.SuspendLayout();
             // 
-            // label1
+            // RentClientComboBox
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Įvedama informacija apie sutartį";
+            this.RentClientComboBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.RentClientComboBox.FormattingEnabled = true;
+            this.RentClientComboBox.Location = new System.Drawing.Point(12, 197);
+            this.RentClientComboBox.Name = "RentClientComboBox";
+            this.RentClientComboBox.Size = new System.Drawing.Size(121, 21);
+            this.RentClientComboBox.TabIndex = 0;
+            this.RentClientComboBox.SelectedIndexChanged += new System.EventHandler(this.RentClientComboBox_SelectedIndexChanged);
             // 
-            // button1
+            // RentWorkerComboBox
             // 
-            this.button1.Location = new System.Drawing.Point(185, 415);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Patvirtinti";
-            this.button1.UseVisualStyleBackColor = true;
+            this.RentWorkerComboBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.RentWorkerComboBox.FormattingEnabled = true;
+            this.RentWorkerComboBox.Location = new System.Drawing.Point(12, 243);
+            this.RentWorkerComboBox.Name = "RentWorkerComboBox";
+            this.RentWorkerComboBox.Size = new System.Drawing.Size(121, 21);
+            this.RentWorkerComboBox.TabIndex = 1;
+            // 
+            // NewRentPriceLine
+            // 
+            this.NewRentPriceLine.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.NewRentPriceLine.Depth = 0;
+            this.NewRentPriceLine.Hint = "";
+            this.NewRentPriceLine.Location = new System.Drawing.Point(12, 270);
+            this.NewRentPriceLine.MaxLength = 32767;
+            this.NewRentPriceLine.MouseState = MaterialSkin.MouseState.HOVER;
+            this.NewRentPriceLine.Name = "NewRentPriceLine";
+            this.NewRentPriceLine.PasswordChar = '\0';
+            this.NewRentPriceLine.SelectedText = "";
+            this.NewRentPriceLine.SelectionLength = 0;
+            this.NewRentPriceLine.SelectionStart = 0;
+            this.NewRentPriceLine.Size = new System.Drawing.Size(75, 23);
+            this.NewRentPriceLine.TabIndex = 2;
+            this.NewRentPriceLine.TabStop = false;
+            this.NewRentPriceLine.Text = "Kaina";
+            this.NewRentPriceLine.UseSystemPasswordChar = false;
+            // 
+            // RentAddConfirmButton
+            // 
+            this.RentAddConfirmButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.RentAddConfirmButton.AutoSize = true;
+            this.RentAddConfirmButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.RentAddConfirmButton.Depth = 0;
+            this.RentAddConfirmButton.Icon = null;
+            this.RentAddConfirmButton.Location = new System.Drawing.Point(12, 299);
+            this.RentAddConfirmButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.RentAddConfirmButton.Name = "RentAddConfirmButton";
+            this.RentAddConfirmButton.Primary = true;
+            this.RentAddConfirmButton.Size = new System.Drawing.Size(103, 36);
+            this.RentAddConfirmButton.TabIndex = 3;
+            this.RentAddConfirmButton.Text = "Patvirtinti";
+            this.RentAddConfirmButton.UseVisualStyleBackColor = true;
+            this.RentAddConfirmButton.Click += new System.EventHandler(this.RentAddConfirmButton_Click);
+            // 
+            // RentAddClientLabel
+            // 
+            this.RentAddClientLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.RentAddClientLabel.AutoSize = true;
+            this.RentAddClientLabel.Depth = 0;
+            this.RentAddClientLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.RentAddClientLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.RentAddClientLabel.Location = new System.Drawing.Point(8, 175);
+            this.RentAddClientLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.RentAddClientLabel.Name = "RentAddClientLabel";
+            this.RentAddClientLabel.Size = new System.Drawing.Size(63, 19);
+            this.RentAddClientLabel.TabIndex = 5;
+            this.RentAddClientLabel.Text = "Klientas";
+            // 
+            // RentAddWorkerLabel
+            // 
+            this.RentAddWorkerLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.RentAddWorkerLabel.AutoSize = true;
+            this.RentAddWorkerLabel.Depth = 0;
+            this.RentAddWorkerLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.RentAddWorkerLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.RentAddWorkerLabel.Location = new System.Drawing.Point(12, 221);
+            this.RentAddWorkerLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.RentAddWorkerLabel.Name = "RentAddWorkerLabel";
+            this.RentAddWorkerLabel.Size = new System.Drawing.Size(69, 19);
+            this.RentAddWorkerLabel.TabIndex = 6;
+            this.RentAddWorkerLabel.Text = "Atstovas";
+            this.RentAddWorkerLabel.Click += new System.EventHandler(this.RentAddWorkerLabel_Click);
             // 
             // Rent_addForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(171, 345);
+            this.Controls.Add(this.RentAddWorkerLabel);
+            this.Controls.Add(this.RentAddClientLabel);
+            this.Controls.Add(this.RentAddConfirmButton);
+            this.Controls.Add(this.NewRentPriceLine);
+            this.Controls.Add(this.RentWorkerComboBox);
+            this.Controls.Add(this.RentClientComboBox);
             this.Name = "Rent_addForm";
             this.Text = "Rent_addForm";
+            this.Load += new System.EventHandler(this.Rent_addForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,7 +141,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox RentClientComboBox;
+        private System.Windows.Forms.ComboBox RentWorkerComboBox;
+        private MaterialSkin.Controls.MaterialSingleLineTextField NewRentPriceLine;
+        private MaterialSkin.Controls.MaterialRaisedButton RentAddConfirmButton;
+        private MaterialSkin.Controls.MaterialLabel RentAddClientLabel;
+        private MaterialSkin.Controls.MaterialLabel RentAddWorkerLabel;
     }
 }
--- WindowsFormsApp1/Rent_editForm.cs
+++ WindowsFormsApp1/Rent_editForm.cs
@@ -7,14 +7,113 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApp1
 {
-    public partial class Rent_editForm : Form
+    public partial class Rent_editForm : MaterialForm
     {
+        public int itemId;
+        public SalesRentForm form;
+        private readonly MaterialSkinManager materialSkinManager;
+
+        public static Form LoginForm;
+
+        public static int userID;
         public Rent_editForm()
         {
             InitializeComponent();
+            materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+            this.Text = "Nuomos sutarties redagavimas";
+        }
+
+        private void Rent_editForm_Load(object sender, EventArgs e)
+        {
+            string cs = Form1.connection;
+
+            var con = new MySqlConnection(cs);
+
+            var commandC = new MySqlCommand("SELECT * FROM klientas", con);
+            var commandW = new MySqlCommand("SELECT * FROM is_vartotojas", con);
+            var adapterC = new MySqlDataAdapter(commandC);
+            var adapterW = new MySqlDataAdapter(commandW);
+            var tableC = new DataTable();
+            var tableW = new DataTable();
+            adapterC.Fill(tableC);
+            RentEditClientBox.DataSource = tableC;
+            RentEditClientBox.DisplayMember = "vardas";
+            RentEditClientBox.ValueMember = "id_Klientas";
+            adapterW.Fill(tableW);
+            RentEditWorkerBox.DataSource = tableW;
+            RentEditWorkerBox.DisplayMember = "vardas";
+            RentEditWorkerBox.ValueMember = "id";
+            Fill_information();
+        }
+
+        private void Fill_information()
+        {
+            string cs = Form1.connection;
+            var con = new MySqlConnection(cs);
+            con.Open();
+            string sql = "SELECT kaina, pastabos, grazinimo_data FROM nuomos_sutartis WHERE id_Nuomos_sutartis = "+itemId+";";
+
+            var cmd = new MySqlCommand(sql, con);
+
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            rdr.Read();
+            RentEditPrice.Text = rdr.GetString(0);
+            if (!rdr.IsDBNull(1))
+            {
+                RentEditPastabos.Text = rdr.GetString(1);
+            }
+            if (!rdr.IsDBNull(2))
+            {
+                RentEditBackDate.Text = rdr.GetString(2);
+            }
+
+            rdr.Close();
+            con.Close();
+        }
+
+        private void RentEditConfirmButton_Click(object sender, EventArgs e)
+        {
+            int client = RentEditClientBox.SelectedIndex + 1;
+            int worker = RentEditWorkerBox.SelectedIndex + 1;
+            double price = -1;
+            Double.TryParse(RentEditPrice.Text, out price);
+            string pastaba = RentEditPastabos.Text; 
+            string data = RentEditBackDate.Text;
+
+            string cs = Form1.connection;
+            var con = new MySqlConnection(cs);
+            con.Open();
+
+            string sql ="UPDATE nuomos_sutartis SET kaina = "+price+", fk_Klientasid = "+client+", fk_ISVartotojas = "+worker;
+
+            if (pastaba != "")
+                sql += ", pastabos = '" + pastaba +"'";
+
+            if (data != "")
+                sql += ", grazinimo_data = " + data;
+
+            sql += " WHERE id_Nuomos_sutartis = "+itemId+";";
+
+            var cmd = new MySqlCommand(sql, con);
+
+            cmd.ExecuteNonQuery();
+
+            form.Refresh_RentListView();
+            this.Close();
+        }
+
+        private void materialLabel5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
